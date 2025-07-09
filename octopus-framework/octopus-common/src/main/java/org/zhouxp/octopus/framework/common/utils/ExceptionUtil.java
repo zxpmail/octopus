cde933eb668f5f3dbee099b64c9e599a45a55414
zhouxp <zxpmail@163.com>
2025-07-09 18:45:39 +0800

    增加异常工具类

---
+++ octopus-framework/octopus-common/src/main/java/org/zhouxp/octopus/framework/common/utils/ExceptionUtil.java
@@ -0,0 +1,72 @@
+package org.zhouxp.octopus.framework.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * <p/>
+ * {@code @description}  : 异常工具类
+ * <p/>
+ * <b>@create:</b> 2025-07-09 11:37:01
+ *
+ * @author zhouxp
+ */
+@Slf4j
+public class ExceptionUtil {
+    private ExceptionUtil() {
+    }
+
+    /**
+     * 将ErrorStack转化为String.
+     */
+    public static String getMessage(Throwable ex) {
+        try (StringWriter stringWriter = new StringWriter();
+             PrintWriter printWriter = new PrintWriter(stringWriter)) {
+            ex.printStackTrace(printWriter);
+            return stringWriter.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Error while getting stack trace as string", e);
+        }
+    }
+
+    /**
+     * 获取组合本异常信息与底层异常信息的异常描述, 适用于本异常为统一包装异常类，底层异常才是根本原因的情况。
+     */
+    public static String getErrorMessageWithNestedException(Throwable ex) {
+        Throwable nestedException = ex.getCause();
+        if (nestedException == null) {
+            return ex.getMessage();
+        }
+        return ex.getMessage() + " nested exception is " +
+                nestedException.getClass().getName() + ":" + nestedException.getMessage();
+    }
+
+    /**
+     * 获取异常的Root Cause.
+     */
+    public static Throwable getRootCause(Throwable ex) {
+        Throwable cause;
+        Throwable result = null;
+        int maxIterations = 20;
+        while ((cause = ex.getCause()) != null && maxIterations-- > 0) {
+            result = cause;
+        }
+        return result;
+    }
+
+
+    /**
+     * 获取确切的异常信息：主要针对代理报错
+     */
+    public static Throwable getActualThrowable(final Throwable throwable) {
+        if (InvocationTargetException.class.equals(throwable.getClass())) {
+            InvocationTargetException exception = (InvocationTargetException) throwable;
+            return exception.getTargetException();
+        }
+        return throwable;
+    }
+}
