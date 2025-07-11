df848f786d66b49cf675a37d5a75a454b0c102b0
zhouxp <zxpmail@163.com>
2025-07-11 19:19:44 +0800

    创建mybatis-plus组件

---
+++ octopus-framework/octopus-spring-boot-starter-mybatis-plus/src/main/java/org/zhouxp/octopus/framework/mybatis/plus/handler/AutoFillMetaObjectHandler.java
@@ -0,0 +1,12 @@
+package org.zhouxp.octopus.framework.mybatis.plus.handler;
+
+/**
+ * <p/>
+ * {@code @description}  :
+ * <p/>
+ * <b>@create:</b> 2025-07-11 19:17:30
+ *
+ * @author zhouxp
+ */
+public class AutoFillMetaObjectHandler {
+}
