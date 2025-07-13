ff4ec64906207d759f1b626f5119d8bf48571412
zhouxp <zxpmail@163.com>
2025-07-13 09:48:19 +0800

    修改填充规则类

---
+++ octopus-framework/octopus-spring-boot-starter-mybatis-plus/src/main/java/org/zhouxp/octopus/framework/mybatis/plus/handler/model/FillRule.java
@@ -0,0 +1,43 @@
+package org.zhouxp.octopus.framework.mybatis.plus.handler.model;
+
+import lombok.Data;
+
+/**
+ * <p/>
+ * {@code @description}  :
+ * <p/>
+ * <b>@create:</b> 2025-07-11 19:26:23
+ *
+ * @author zhouxp
+ */
+@Data
+public class FillRule {
+    /**
+     * 填充字段名
+     */
+    private String fieldName;
+    /**
+     * 填充字段类型 类型字符串，如 java.lang.String
+     */
+    private String fieldType;
+    /**
+     * 插入时填充
+     */
+    private Boolean insertOnly;
+    /**
+     * 更新时填充
+     */
+    private Boolean updateOnly;
+    /**
+     * 填充数据源类型 header / param / default
+     */
+    private String sourceType;
+    /**
+     * 填充数据源字段名  如 userID
+     */
+    private String sourceKey;
+    /**
+     * 默认值
+     */
+    private String defaultValue;
+}
