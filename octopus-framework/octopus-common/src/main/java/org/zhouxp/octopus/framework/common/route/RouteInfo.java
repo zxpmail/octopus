8adfecc7413152823216b342dea9fbe2fe2af87f
zhouxp <zxpmail@163.com>
2025-07-23 16:17:06 +0800

    增加路由信息类

---
+++ octopus-framework/octopus-common/src/main/java/org/zhouxp/octopus/framework/common/route/RouteInfo.java
@@ -0,0 +1,11 @@
+package org.zhouxp.octopus.framework.common.route;
+
+/**
+ * <p/>
+ * {@code @description}  : 路由信息类
+ * <p/>
+ * <b>@create:</b> 2025-07-23 16:09:23
+ *
+ * @author zhouxp
+ */
+public record RouteInfo(String ip, Integer port) {}
