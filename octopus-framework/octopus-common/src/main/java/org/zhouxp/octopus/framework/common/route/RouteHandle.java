526143646891ec568cfe993be25888e5306fbcec
zhouxp <zxpmail@163.com>
2025-07-23 16:05:25 +0800

    增加路由处理接口

---
+++ octopus-framework/octopus-common/src/main/java/org/zhouxp/octopus/framework/common/route/RouteHandle.java
@@ -0,0 +1,15 @@
+package org.zhouxp.octopus.framework.common.route;
+
+import java.util.List;
+
+/**
+ * <p/>
+ * {@code @description}  : 路由处理接口
+ * <p/>
+ * <b>@create:</b> 2025-07-23 16:03:25
+ *
+ * @author zhouxp
+ */
+public interface RouteHandle {
+    String routeServer(List<String> values, String key);
+}
